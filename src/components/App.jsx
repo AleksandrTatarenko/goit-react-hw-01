02ac5d36311b8c5c82a3b1b94ccda5e3f15aef80
Aleksandr Tatarenko <aleksandrcrimea96@gmail.com>
2022-10-05 21:35:30 +0300

    hw

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,16 +9,7 @@ import transactions from "path/to/transactions.json";
 
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'block',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
+    <div>
       <Profile
         username={user.username}
         tag={user.tag}
